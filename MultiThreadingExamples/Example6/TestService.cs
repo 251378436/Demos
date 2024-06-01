6796574a89154d6fca39d01f7e33607dc125a024
251378436 <zjgslongbo1@gmail.com>
2024-06-01 22:36:20 +1200

    add multi threading example

---
+++ MultiThreadingExamples/Example6/TestService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example6;
+
+internal class TestService
+{
+    private readonly HttpClient httpClient;
+    public TestService() 
+    { 
+        httpClient = new HttpClient()
+        {
+            BaseAddress = new Uri("http://localhost:5022/"),
+        };
+    }
+
+    public async Task Process(int key)
+    {
+        Console.WriteLine($"Starting {key} with thread {Thread.CurrentThread.ManagedThreadId} *******");
+        await SendRequest();
+        Console.WriteLine($"Finished {key} with thread {Thread.CurrentThread.ManagedThreadId} *******");
+    }
+
+    private async Task SendRequest()
+    {
+        var response = await httpClient.GetAsync("values");
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+    }
+}
