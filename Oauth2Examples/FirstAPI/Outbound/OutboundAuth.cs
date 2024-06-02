33911aa417edce35de8be51a753859b0fe5a7eec
251378436 <zjgslongbo1@gmail.com>
2024-06-03 10:29:10 +1200

    update property name to domain

--- Oauth2Examples/FirstAPI/Outbound/OutboundAuth.cs
+++ Oauth2Examples/FirstAPI/Outbound/OutboundAuth.cs
@@ -2,7 +2,7 @@
 
 public class OutboundAuth
 {
-    public virtual string Issuer { get; set; }
+    public virtual string Domain { get; set; }
     public virtual string? Audience { get; set; }
     public virtual string? ClientId { get; set; }
     public virtual string? ClientSecret { get; set; }
